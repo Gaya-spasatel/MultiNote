844bd254ed7dfe9f6506e0c049aec37ce3adcd3e
Gaya Peregudova <dashapereg@mail.ru>
2021-05-28 15:43:17 +0300

    Done async

--- app/src/main/java/com/peregudova/multinote/LoginViewModel.java
+++ app/src/main/java/com/peregudova/multinote/LoginViewModel.java
@@ -1,5 +1,7 @@
 package com.peregudova.multinote;
 
+import android.os.AsyncTask;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -12,19 +14,37 @@ import com.peregudova.multinote.requests.User;
 import java.io.IOException;
 
 public class LoginViewModel extends ViewModel {
-
+    private LoginAnswer answer;
     private MutableLiveData<Boolean> showProgress = new MutableLiveData<>();
+
+    class LoginTask extends AsyncTask<User, Void, LoginAnswer>{
+
+        @Override
+        protected LoginAnswer doInBackground(User... users) {
+            LoginAnswer loginAnswer = null;
+            for(User user:users){
+                try {
+                    loginAnswer = new Requester().loginUser(user);
+                } catch (IOException e) {
+                    //e.printStackTrace();
+                }
+            }
+            return loginAnswer;
+        }
+
+        @Override
+        protected void onPostExecute(LoginAnswer loginAnswer) {
+            answer = loginAnswer;
+        }
+    }
+
     public LoginAnswer logIn(String login, String password){
         showProgress.postValue(true);
         //логика авторизации
-        try {
-            LoginAnswer loginAnswer = new Requester().loginUser(new User(login,password));
-            showProgress.postValue(false);
-            return loginAnswer;
-        } catch (IOException e) {
-            showProgress.postValue(false);
-            return new LoginAnswer("Error", null, null);
-        }
+        //LoginAnswer loginAnswer =
+        LoginTask loginTask = (LoginTask) new LoginTask().execute(new User(login, password));
+        showProgress.postValue(false);
+        return answer;
     }
 
     public MutableLiveData<Boolean> getProgressState(){
