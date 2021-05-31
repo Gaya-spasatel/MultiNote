dd6b8d9032330357e14686290133883adfef89ea
Gaya Peregudova <dashapereg@mail.ru>
2021-05-31 13:44:06 +0300

    Проверщик имэйлов

--- app/src/main/java/com/peregudova/multinote/checkers/EmailChecker.java
+++ app/src/main/java/com/peregudova/multinote/checkers/EmailChecker.java
@@ -1,8 +1,11 @@
 package com.peregudova.multinote.checkers;
 
+import java.util.regex.Pattern;
+
 public class EmailChecker implements Checker{
     @Override
     public boolean check(String line) {
-        return false; // TODO: 25.05.2021 реализовать проверку на соотвествие паттерну емайл 
+        if(line.length()<5) return false;
+        return Pattern.compile("[\\d\\w]+@[\\d\\w]+.[\\d\\w]{2,3}").matcher(line).matches();
     }
 }
